bc406fb68a4002100ec3f69a97882b7ca519077d
Marin Marinov <marin.marinov@macaulay.cuny.edu>
2022-05-07 10:46:40 -0400

    testing out controller context

--- phx_any/hello/lib/hello_web/controllers/page_controller.ex
+++ phx_any/hello/lib/hello_web/controllers/page_controller.ex
@@ -1,7 +1,14 @@
 defmodule HelloWeb.PageController do
   use HelloWeb, :controller
 
+  plug(:tester)
+
   def index(conn, _params) do
     render(conn, "index.html")
+  end
+
+  defp tester(conn, _params) do
+    IO.puts("I am middleware!")
+    conn
   end
 end
